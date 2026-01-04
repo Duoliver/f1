8721a023b2435bce586d592f7b7fefedcb60b4ef
Duoliver <oliveira.douglasedu@outlook.com>
2026-01-04 15:07:27 -0300

    make seasons page grid responsive

--- app/src/routes/seasons/index.tsx
+++ app/src/routes/seasons/index.tsx
@@ -11,7 +11,7 @@ function SeasonsPage() {
   return (
     <div className="flex flex-col items-center gap-16">
       <h1>Formula One Seasons</h1>
-      <div className="grid grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-5 gap-4">
         {seasons.map(({ year }) => (
           <h2 className="text-center" key={year}>
             {year}

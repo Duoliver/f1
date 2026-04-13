6a0d74ba68912709b1690bf815c269921812e365
Duoliver <oliveira.douglasedu@outlook.com>
2026-04-13 14:57:32 -0300

    render races in season page

---
+++ app/src/components/RaceCard/index.tsx
@@ -0,0 +1,44 @@
+import type Race from '../../types/response/Race';
+
+interface RaceCardProps {
+  race: Race; // not the best option
+}
+
+// TODO convert date to MONTH 10TH
+// TODO parse string IDs to names
+
+export default function RaceCard({ race }: RaceCardProps) {
+  return (
+    <section className="flex flex-col p-4 gap-12 border border-white">
+      <header className="flex flex-col gap-2">
+        <div className="flex justify-between">
+          <h6>Round {race.round}</h6>
+          <p className="p-0">{race.date}</p>
+        </div>
+        <h3 className="text-yellow">{race.grandPrixId}</h3>
+        <p className="p-0 uppercase">{race.circuitId}</p>
+      </header>
+      <footer className="flex flex-col">
+        {race.raceResults.slice(0, 3).map((driverResult) => (
+          <div
+            className="flex gap-1"
+            key={`${race.grandPrixId}-${driverResult.positionNumber}`}
+          >
+            <span className="flex items-center justify-center text-center aspect-square h-9 bg-yellow">
+              <h5 className="text-black">{driverResult.positionNumber}</h5>
+            </span>
+            <div>
+              <h6>{driverResult.driverId}</h6>
+              <p className="p-0 text-yellow uppercase">
+                {driverResult.time ||
+                  driverResult.gap ||
+                  driverResult.reasonRetired ||
+                  '-'}
+              </p>
+            </div>
+          </div>
+        ))}
+      </footer>
+    </section>
+  );
+}

0f0af06ae18f04a57777faf676abee554c4d0471
Duoliver <oliveira.douglasedu@outlook.com>
2026-04-15 12:55:20 -0300

    display driver result time only for the winner and gap/dnf reason for the others

--- app/src/utils/getDriverResultTimeOrStatus.ts
+++ app/src/utils/getDriverResultTimeOrStatus.ts
@@ -3,7 +3,6 @@ import type RaceDriverResult from '../types/response/RaceDriverResult';
 export default function getDriverResultTimeOrStatus(
   driverResult: RaceDriverResult
 ) {
-  return (
-    driverResult.time || driverResult.gap || driverResult.reasonRetired || '-'
-  );
+  if (driverResult.positionNumber === 1) return driverResult.time;
+  return driverResult.gap || driverResult.reasonRetired || '-';
 }

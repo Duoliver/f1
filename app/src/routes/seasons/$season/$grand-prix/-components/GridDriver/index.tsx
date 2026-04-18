b4cedb2d4fc08df2cd66164ce344c61622dce39b
Duoliver <oliveira.douglasedu@outlook.com>
2026-04-18 18:55:06 -0300

    style grid driver

--- app/src/routes/seasons/$season/$grand-prix/-components/GridDriver/index.tsx
+++ app/src/routes/seasons/$season/$grand-prix/-components/GridDriver/index.tsx
@@ -23,12 +23,27 @@ export default function GridDriver({
       : `${clearSlug(constructor)}-${clearSlug(engineManufacturer)}`;
 
   return (
-    <>
-      <span data-testid="grid-driver-position">{position || '-'}</span>
+    <div className="grid grid-cols-[3.75rem_minmax(0,1fr)] grid-rows-2 gap-x-2 gap-y-0">
+      <div
+        data-testid="grid-driver-position"
+        className="row-span-2 flex items-center justify-center aspect-square bg-yellow w-[3.75rem]"
+      >
+        <h3 className="text-center text-black">{position || '-'}</h3>
+      </div>
       <h4 data-testid="grid-driver-name">{clearSlug(driverName)}</h4>
-      <div>
-        <span data-testid="grid-driver-number">{carNumber}</span>
-        <span data-testid="grid-driver-constructor">{fullConstructor}</span>
+      <div className="col-start-2 row-start-2 flex items-center gap-2">
+        <span
+          data-testid="grid-driver-number"
+          className="flex items-center justify-center h5 text-black w-12 h-7 rounded-[100%] bg-cyan"
+        >
+          {carNumber}
+        </span>
+        <span
+          data-testid="grid-driver-constructor"
+          className="text-cyan text-2xl uppercase"
+        >
+          {fullConstructor}
+        </span>
         <span
           data-testid="grid-driver-tyre-manufacturer"
           title={capitalize(tyreManufacturer)}
@@ -36,7 +51,7 @@ export default function GridDriver({
           {tyreManufacturer[0]}
         </span>
       </div>
-    </>
+    </div>
   );
 }
 

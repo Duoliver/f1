12da48e0a74a5e09972735fb98151fed5c9493b9
Duoliver <oliveira.douglasedu@outlook.com>
2026-04-21 13:28:04 -0300

    extract season grand prix header into component

---
+++ app/src/routes/seasons/$season/$grandPrix/-components/SeasonGrandPrixPageHeader/index.tsx
@@ -0,0 +1,31 @@
+import { formatFullDate } from '../../../../../../utils/formatDate';
+import type SeasonGrandPrixPageHeaderProps from './types';
+
+export default function SeasonGrandPrixPageHeader({
+  circuitName,
+  date,
+  round,
+}: SeasonGrandPrixPageHeaderProps) {
+  return (
+    <div className="flex flex-col md:flex-row justify-between items-center">
+      <span
+        data-testid="season-grand-prix-round"
+        className="flex-1 text-center md:text-start"
+      >
+        Round {round}
+      </span>
+      <span
+        data-testid="season-grand-prix-circuit"
+        className="flex-1 uppercase text-center"
+      >
+        {circuitName}
+      </span>
+      <span
+        data-testid="season-grand-prix-date"
+        className="flex-1 uppercase text-center md:text-end"
+      >
+        {formatFullDate(date)}
+      </span>
+    </div>
+  );
+}
---
+++ app/src/routes/seasons/$season/$grandPrix/-components/SeasonGrandPrixPageHeader/types.ts
@@ -0,0 +1,5 @@
+export default interface SeasonGrandPrixPageHeaderProps {
+  round: number;
+  circuitName: string;
+  date: string;
+}

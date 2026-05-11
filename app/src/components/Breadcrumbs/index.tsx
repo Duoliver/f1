a345d6522fee2d727c184357d131e67784d775eb
Duoliver <oliveira.douglasedu@outlook.com>
2026-05-11 18:07:47 -0300

    create breadcrumbs component

---
+++ app/src/components/Breadcrumbs/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from '@tanstack/react-router';
+import type BreadcrumbsProps from './types';
+import { Fragment } from 'react/jsx-runtime';
+
+export default function Breadcrumbs({ links }: BreadcrumbsProps) {
+  return (
+    <menu className="capitalize" data-testid="breadcrumbs">
+      {links.slice(0, -1).map((linkOptions) => (
+        <Fragment key={linkOptions.to}>
+          <Link {...linkOptions} className="text-yellow">
+            {linkOptions.label}
+          </Link>
+          <span className="after:content-['_>_']" />
+        </Fragment>
+      ))}
+      {links.at(-1)!.label}
+    </menu>
+  );
+}
